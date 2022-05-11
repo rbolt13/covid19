ba4814947bd0b9bd54cc0c85d6aa7c894c3ca07b
rbolt13 <rbolt@pdx.edu>
2022-05-11 09:50:50 -0700

    added Makefile and raw data

---
+++ Makefile
@@ -0,0 +1,3 @@
+# Save data to raw_data folder
+raw_data/data.rds:: R/00_save_data.R
+	Rscript R/00_save_data.R
--- R/00_save_data.R
+++ R/00_save_data.R
@@ -1,11 +1,17 @@
 here::i_am("R/00_save_data.R")
 
+library(readr)
+
 # Read Data
 us_states_covid_data <- readr::read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 us_counties <- readr::read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 vacc <- readr::read_csv("https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/us_data/time_series/people_vaccinated_us_timeline.csv")
 
+# location of data
+location_of_data <- here::here("raw_data", "data.rds")
 
-# save data list
+# put data into list
 data_list <- list(us_states_covid_data, us_counties, vacc)
-saceRDS(data_list, "raw_data/data.rds")
+
+# save list of data
+saveRDS(data_list, location_of_data)
