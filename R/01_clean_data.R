580845d9cdd287b157de7af867ed3fa1b1b34d8d
rbolt13 <rbolt@pdx.edu>
2022-05-12 10:02:47 -0700

    update clean data code

--- R/01_clean_data.R
+++ R/01_clean_data.R
@@ -1,3 +1,8 @@
+#' Clean Data
+#' 
+#' @description 
+#'
+
 here::i_am("R/01_clean_data")
 
 library(dplyr)
@@ -20,11 +25,14 @@ vacc <- readRDS(location_of_vacc)
 state_pop <- readRDS(location_of_state_pop)
 or_pop <- readRDS(location_of_or_pop)
 
-# rename columns
+# rename columns (for future join)
 state_pop <- dplyr::rename("state" = NAME,
                            "population" = value)
 or_pop <- dplyr::rename("county" = NAME,
                         "population" = value)
+
+# remove " County, Oregon" from the county name (for future join)
+or_pop <- gsub(" County, Oregon", "", or_pop)
 
 # join state covid data with vacc data
 state_join <- states_covid %>%
@@ -51,8 +59,21 @@ state_join <- states_covid %>%
                      ), 
             by = c("state" = "state")
             ) 
+
+# join counties covid data with or pop data
+join_or <- counties_covid %>%
+  select(date, 
+         county, 
+         cases, 
+         deaths
+         ) %>%
+  full_join(select(county,
+                   population),
+            by = c("county" = "county")
+  )
+
 # clean state data
-state_covid_clean <- state_join %>%
+us_covid_clean <- state_join %>%
   summarise(date = date,
             state = state,
             population = population,
@@ -65,10 +86,23 @@ state_covid_clean <- state_join %>%
             partial_vacc = People_Partially_Vaccinated,
             partial_vacc_per_pop = partial_vacc/population)
 
-# join counties covid data with or pop data
-
+# clean or data
+or_covid_clean <- join_or %>%
+  summarise(date = date,
+            county = county,
+            population = population,
+            cases = cases,
+            cases_per_pop = cases/population,
+            deaths = deaths,
+            deaths_per_pop = deaths/population)
 
 # location of cleaned data
+location_of_us_clean <- here::here("clean_data",
+                                      "us_covid_clean.rds")
 
+location_of_or_clean <- here::here("clean_data",
+                                   "or_covid_clean.rds")
 
 # saved data
+saveRDS(us_covid_clean, location_of_us_clean)
+saveRDS(or_covid_clean, location_of_or_clean)
